22c922581fada6e3d6b5b8084ce38a63731dedc8
Gourav Kumar <33678627+unpredictablegourav@users.noreply.github.com>
2019-11-14 00:09:54 +0530

    Adding_Script

---
+++ bulkVM_creation.ps1
@@ -0,0 +1,14 @@
+for ($i=1; $i -le 3; $i++)
+{
+    New-AzureRMVm `
+        -ResourceGroupName "myResourceGroupLoadBalancer" `
+        -Name "myVM$i" `
+        -Location "East US" `
+        -VirtualNetworkName "myVnet" `
+        -SubnetName "mySubnet" `
+        -SecurityGroupName "myNetworkSecurityGroup" `
+        -OpenPorts 80 `
+        -AvailabilitySetName "myAvailabilitySet" `
+        -Credential $cred `
+        -AsJob
+}
